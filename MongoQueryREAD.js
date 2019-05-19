b0f281e30e3d0bd2fd3cb40922285979bce4021f
ahsanhabib91 <habibibnakther@gmail.com>
2019-05-19 18:04:05 +0600

    Done Logical Operators

--- MongoQueryREAD.js
+++ MongoQueryREAD.js
@@ -1,7 +1,6 @@
 // 07 Read Operations - A Closer Look
 // The queries are based on tv-shows.json
 
-
 // Query Selectors: https://docs.mongodb.com/manual/reference/operator/query/#query-selectors
 
 // Comparison Operators
@@ -13,3 +12,19 @@ db.movies.find({ genres: "Drama" }) // whether Drama exists in genres array
 db.movies.find({ genres: [ "Drama" ] }) // genres is exactly array [ "Drama" ]
 
 // Logical Operators
+db.movies.find({ $or: [ {"rating.average": {$lt: 5}}, {"rating.average": {$gt: 9.3}} ] }) // match documents (<5 or >9.3)
+db.movies.find({ $nor: [ {"rating.average": {$lt: 5}}, {"rating.average": {$gt: 9.3}} ] }) // match documents (!<5 or !>9.3). i.e. (>5 and <9.3). $nor	Joins query clauses with a logical NOR returns all documents that fail to match both clauses. i.e. neither condition met.
+db.movies.find({ $and: [ {"rating.average": {$gt: 9}}, {"genres": "Drama"} ] }) // match documents (rating.average>9 and "Drama" in genres)
+// another way: 
+db.movies.find({"rating.average": {$lt: 5}, "genres": "Drama"}) // match documents (rating.average>9 and "Drama" in genres). Default to and
+/**
+ * But, for same Fields like "genres",
+ * db.movies.find({"genres": "Drama", "genres": "Action"}) === db.movies.find({"genres": "Action"}). 
+ * Therefore to get the correct results, this needs to be done using the $and operator
+ * db.movies.find({ $and: [ {"genres": "Drama"}, {"genres": "Action"} ] })
+ */
+db.movies.find({ runtime: { $not: {$eq: 60} } }) // match documents where, runtime !== 60
+// it also equvalent to: db.movies.find({ runtime: {$ne: 60} })
+
+// Element Operators
+
