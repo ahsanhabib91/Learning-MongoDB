a25092fcfba5314451262edf510449ec1ad7387c
ahsanhabib91 <habibibnakther@gmail.com>
2019-06-26 18:05:09 +0600

    Started week 1. learning  stage

---
+++ MongoDBUniversity/The-MongoDB-Aggregation-Framework/week2.js
@@ -0,0 +1,10 @@
+db.movies.aggregate([
+    {
+        $group: { 
+            _id: "$year", 
+            num_films_in_year: { $sum: 1 }
+        },
+    }, {
+        $sort: { num_films_in_year: -1 }
+    }
+])
